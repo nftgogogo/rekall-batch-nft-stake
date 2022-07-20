faa89d8506505ee2ac3ae45bc53bfcda342135d6
nftgogogo <rekallbatchnftstake@gmail.com>
2022-07-20 17:45:27 +0800

    change bscFastGasPrice and bscGasLimit

--- app/src/main/java/onekey/rekallutils/base/BaseConfig.kt
+++ app/src/main/java/onekey/rekallutils/base/BaseConfig.kt
@@ -4,11 +4,9 @@ import java.math.BigInteger
 
 class BaseConfig {
     companion object{
-        val bscFastGasPrice: BigInteger = BigInteger.valueOf(20)
-        val bscGasPrice: BigInteger = BigInteger.valueOf(20)
-        val bscGasLimit: BigInteger = BigInteger.valueOf(300000)
+        val bscFastGasPrice: BigInteger = BigInteger.valueOf(5)
+        val bscGasLimit: BigInteger = BigInteger.valueOf(500000)
         val chainId: Long = 56
-        val testChainId: Long = 97
         val EKADecimal:Int = 18
         val BNBDecimal:Int = 18
         val EKAContractAddress:String  = "0x9533fa068c12d03011ea15dc64d382930f15593d"
